7cacc76706bbdda0bc157980aab7c07f3fd89320
Vladislav Ivanov <vladislav_ivanovm@mail.ru>
2025-05-04 19:29:56 +0300

    Update ProfileFragment.java

--- movieapp/ProfileFragment.java
+++ movieapp/ProfileFragment.java
@@ -10,7 +10,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import com.example.moviecollection.databinding.FragmentProfileBinding;
+import com.example.movieapp.databinding.FragmentProfileBinding;
 
 public class ProfileFragment extends Fragment {
 
